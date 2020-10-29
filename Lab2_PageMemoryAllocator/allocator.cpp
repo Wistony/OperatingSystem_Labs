54e143a0e6fa5a9d8c36888e38849d7cc51e5779
Max.Androshchuk <mmaksimm921@gmail.com>
2020-10-30 00:37:06 +0200

    class and struct for allocator

--- Lab2_PageMemoryAllocator/allocator.cpp
+++ Lab2_PageMemoryAllocator/allocator.cpp
@@ -1,16 +1,60 @@
 #include "allocator.h"
+#include <iostream>
 
 uint8_t Allocator::memory[MEMORY_SIZE];
 const size_t Allocator::pageNum;
+map<void*, PageHeader> Allocator::pageHeaders;
+vector<void*> Allocator::freePages;
+map<int, vector<void*>> Allocator::classifiedPages;
+
+Allocator::Allocator() 
+{
+	initializePageHeaders();
+	initializeFreePages();
+	initializeClassifiedPages();
+}
+
+void Allocator::initializePageHeaders() 
+{
+	uint8_t* pagePtr = (uint8_t*)memory;
+
+	for (int i = 0; i < pageNum; i++)
+	{
+		PageHeader header = { Free, 0, 0, NULL };
+		pageHeaders.insert({pagePtr, header});
+		pagePtr += PAGE_SIZE;
+	}
+}
+
+void Allocator::initializeFreePages() 
+{
+	uint8_t* pagePtr = (uint8_t*)memory;
+
+	for (int i = 0; i < pageNum; i++)
+	{
+		freePages.push_back(pagePtr);
+		pagePtr += PAGE_SIZE;
+	}
+}
+
+void Allocator:: initializeClassifiedPages()
+{
+	int classSize = 8;
+
+	for (; classSize <= PAGE_SIZE / 2; classSize >>= 1)
+	{
+		classifiedPages.insert({ classSize, {} });
+	}
+}
 
 void* Allocator::mem_alloc(size_t size)
 {
-
+	return NULL;
 }
 
 void* Allocator::mem_realloc(void* address, size_t size)
 {
-
+	return NULL;
 }
 
 void Allocator::mem_free(void* address) 
--- Lab2_PageMemoryAllocator/allocator.h
+++ Lab2_PageMemoryAllocator/allocator.h
@@ -1,13 +1,39 @@
 #include <cstdint>
+#include <vector>
+#include <map>
+
 using namespace std;
 
 #define MEMORY_SIZE 4096
 #define PAGE_SIZE 256
 
+enum PageState
+{
+	Free,
+	DividedIntoBlocks,
+	MultiPageBlocks
+};
+
+struct PageHeader 
+{
+	PageState state;
+	size_t classSize;
+	size_t freeBlockAmount;
+	uint8_t* freeBlockPtr;
+};
+
 class Allocator 
 {
+private:
 	static uint8_t memory[MEMORY_SIZE];
 	static const size_t pageNum = MEMORY_SIZE / PAGE_SIZE;
+	static map <void*, PageHeader> pageHeaders;
+	static vector <void*> freePages;
+	static map <int, vector<void*>> classifiedPages;
+
+	void initializeFreePages();
+	void initializePageHeaders();
+	void initializeClassifiedPages();
 
 public: 
 	Allocator();
