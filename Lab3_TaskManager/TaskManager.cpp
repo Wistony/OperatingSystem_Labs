fb0364799353974f687cfa60905baf71c7661886
Wistony <mmaksimm921@gmail.com>
2020-12-10 01:05:49 +0200

    Functions for calculating metrics added

--- Lab3_TaskManager/TaskManager.cpp
+++ Lab3_TaskManager/TaskManager.cpp
@@ -7,16 +7,17 @@ TaskManager::TaskManager()
 {
 	task_queue = vector<queue<Task>>(32);
 	currentMaxPriority = 0;
+	taskEnded = 0;
+	taskInQueue = 0;
 }
 
 void TaskManager::AddTask(Task task) 
 {
 	_lock.lock();
-
-	cout << " New task ADDED: Id - " << task.Id << ", Dur: " << task.Duration << " ms, Pr: " << task.Priority << endl;
+	cout << " New task ADDED: Id: " << task.Id << ", Dur: " << task.Duration << " ms, Pr: " << task.Priority << endl;
 	task_queue[task.Priority - 1].push(task);
+	taskInQueue += 1;
 	findMaxPriority();
-
 	_lock.unlock();
 }
 
@@ -52,12 +53,12 @@ void TaskManager::ExecuteTask()
 
 	_lock.lock();
 	task_queue[currentMaxPriority - 1].pop();
+	taskInQueue -= 1;
 	_lock.unlock();
 
 	findMaxPriority();
 	
 	cout << " Execution STARTED: Id: " << task.Id << ", Dur: " << task.Duration << " ms, Pr: " << task.Priority << endl;
-
 
 	int duration = task.Duration;
 	while (duration != 0)
@@ -68,6 +69,17 @@ void TaskManager::ExecuteTask()
 			duration -= 5;
 			if (currentMaxPriority != 0 && currentMaxPriority < task.Priority) 
 			{
+				Task tsk = task;
+				if (waitingTime.empty())
+				{
+					tsk.Duration = 0;
+				}
+				else 
+				{
+					tsk.Duration = waitingTime[waitingTime.size() - 1].Duration + task.Duration - duration;
+				}
+				waitingTime.push_back(tsk);
+
 				task.Duration = duration;
 				_lock.lock();
 				cout << " Execution STOPED: Id: " << task.Id << ", Dur left: " << task.Duration << " ms, Pr: " << task.Priority << endl;
@@ -79,15 +91,86 @@ void TaskManager::ExecuteTask()
 		else
 		{
 			Sleep(task.Duration);
+			Task tsk = task;
+			if (waitingTime.empty())
+			{
+				tsk.Duration = 0;
+			}
+			else
+			{
+				tsk.Duration = waitingTime[waitingTime.size() - 1].Duration + task.Duration;
+			}
+			waitingTime.push_back(tsk);
 			duration = 0;
 			_lock.lock();
 			cout << " Execution ENDED: Id: " << task.Id << ", Dur: " << task.Duration << " ms, Pr: " << task.Priority << endl;
 			_lock.unlock();
-
+			taskEnded += 1;
 		}
 
 	};
+	
 	return;
+}
+
+vector<int> TaskManager::calculateAverageWaitingTimeByPriority()
+{	
+	vector<int> averageWaitingTime(MIN_PRIORITY);
+
+	for (int i = 0; i < MIN_PRIORITY; i++) 
+	{
+		int sum = 0;
+		int count = 0;
+		for (int j = 0; j < waitingTime.size(); j++) 
+		{
+			if (waitingTime[j].Priority == i + 1)
+			{
+				count += 1;
+				sum += waitingTime[j].Duration;
+			}
+		}
+
+		if (count != 0) 
+		{
+			averageWaitingTime[i] = sum / count;
+		}
+		else 
+		{
+			averageWaitingTime[i] = 0;
+		}
+	}
+
+	return averageWaitingTime;
+}
+
+int TaskManager::calculateAverageWaitingTime() 
+{
+	return calculateWaitingTime() / waitingTime.size();
+}
+
+int TaskManager::calculateWaitingTime() 
+{
+	int sum = 0;
+	for (int i = 0; i < MIN_PRIORITY; i++)
+	{
+		for (int j = 0; j < waitingTime.size(); j++)
+		{
+			sum += waitingTime[j].Duration;
+		}
+	}
+
+	return sum;
+}
+
+bool TaskManager::isEnd() 
+{
+	return taskEnded == TASK_COUNT;
+}
+
+
+bool TaskManager::queueIsEmpty() 
+{
+	return taskInQueue == 0;
 }
 
 void TaskManager::dump() 
--- Lab3_TaskManager/TaskManager.h
+++ Lab3_TaskManager/TaskManager.h
@@ -1,12 +1,15 @@
 #include <queue>
 #include <iostream>
 #include <mutex>
+#include <vector>
+#include <numeric>
 using namespace std;
 
 #define MAX_PRIORITY 1
-#define MIN_PRIORITY 4
-#define TASK_COUNT 5
-#define MAX_DURATION 100
+#define MIN_PRIORITY 8
+#define TASK_COUNT 20
+#define MAX_DURATION 50
+#define INTENSITY 400
 
 
 
@@ -37,12 +40,22 @@ class TaskManager
 private:
 	recursive_mutex _lock;
 	vector<queue<Task>> task_queue;
+	vector<Task> waitingTime;
+
 	int currentMaxPriority;
+	int taskEnded;
+	int taskInQueue;
+
+	void findMaxPriority();
 
 public: 
 	TaskManager();
 	void AddTask(Task);
 	void ExecuteTask();
-	void findMaxPriority();
 	void dump();
+	vector<int> calculateAverageWaitingTimeByPriority();
+	int calculateWaitingTime();
+	int calculateAverageWaitingTime();
+	bool isEnd();
+	bool queueIsEmpty();
 };
